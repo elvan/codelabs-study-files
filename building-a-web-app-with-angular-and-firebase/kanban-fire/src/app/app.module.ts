d461695e9cd3fc0369470c7a312689c8c76e4609
Elvan H <elvanhd@gmail.com>
2020-12-20 20:09:57 +0700

    visualizing tasks

--- building-a-web-app-with-angular-and-firebase/kanban-fire/src/app/app.module.ts
+++ building-a-web-app-with-angular-and-firebase/kanban-fire/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TaskComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
